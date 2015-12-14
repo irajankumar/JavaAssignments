716a70995cc52057bb79f845ca4fe3ca45e3743a
rkking <rkking@live.com>
2015-12-14 16:42:55 +0530

    Sum using command Line

---
+++ CmdSum.java
@@ -0,0 +1,7 @@
+class CmdSum{
+	// This is a progra to take arguments from command line and find out their sum
+	public static void main(String []args){	
+		System.out.println("The sum of Entered numbers is " + (Integer.parseInt(args[0])+Integer.parseInt(args[1])));
+	
+	}
+}
